891f31b63f40ba8a2c1469b2626acf34edddcc08
CHABCHI Nassim <57442076+thatbreezeofmine@users.noreply.github.com>
2022-06-01 19:51:48 +0200

    first commit, updated the requirements.txt
    For now, I believe I will only need pandas and dash as new imported modules.
    
    Quick explanation: the dashboard.py will be the main module, this will start the dashboard on the browser.
    
    P.S the test_0_database_model.py was updated (4 new lines added I believe) because I encountered an error during my first launch of the program. It wouldn't detect the ctr module in the parent directory.

--- tests/test_0_database_model.py
+++ tests/test_0_database_model.py
@@ -1,3 +1,8 @@
+import os, sys
+currentdir = os.path.dirname(os.path.realpath(__file__))
+parentdir = os.path.dirname(currentdir)
+sys.path.append(parentdir)
+
 from ctr.Util.Util import Util
 from ctr.Util import global_config
 from ctr.Database.connection import SqlConnector

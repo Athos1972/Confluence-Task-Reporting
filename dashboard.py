b85e96be619b78c331c9eb2ed29befbbd79d36b1
CHABCHI Nassim <57442076+thatbreezeofmine@users.noreply.github.com>
2022-06-03 10:03:00 +0100

    - Done most of the stuff :
    * Fixed the prefill_database.py
    * added a company field in the user model (should be added to your specific database when using). I couldn't figure out how to use the hybrid company_name field. If it can be used then no need for the company field I added.
    P.S note that the init_user_companies function in explorer.py should be dismissed in case we use the hybrid company_name.
    * Added company specific queries in reporter.py
    
    P.S The code will be commented once the Grid added.

--- dashboard.py
+++ dashboard.py
@@ -1,6 +1,6 @@
 import dash
 import dash_bootstrap_components as dbc
-from dash import Input, Output, State, html, dcc
+from dash import Input, Output, html, dcc, callback_context
 from ctr.Database.connection import SqlConnector
 import ctr.Reporting.Reporter as Reporter
 import ctr.Exploring.Explorer as Explorer
@@ -13,20 +13,45 @@ explorer = Explorer.TaskExploring(db_connection)
 
 companies = explorer.get_companies()
 spaces = explorer.get_spaces()
+explorer.init_user_companies()
+
 open_tasks_per_space = reporter.task_count_by_space()
 open_overdue_tasks_per_space = reporter.task_overdue_count_by_space()
-
+open_tasks_per_company = reporter.task_count_by_company()
+open_overdue_tasks_per_company = reporter.task_overdue_count_by_company()
 open_tasks_per_space_data = open_tasks_per_space
+open_tasks_per_company_data = open_tasks_per_company
 
-print(open_overdue_tasks_per_space, open_tasks_per_space)
+space_overdue_tasks_card = dbc.Card(
+    dbc.CardBody(
+        [
+            html.H4("Space's Overdue Tasks", className="card-title"),
+            html.P(html.Center(html.Strong("-")),
+                   className="card-text",
+                   ),
+        ]
+    ),
+    className="w-100",
+)
+
+company_overdue_tasks_card = dbc.Card(
+    dbc.CardBody(
+        [
+            html.H4("Company's Overdue Tasks", className="card-title"),
+            html.P(html.Center(html.Strong("-")),
+                   className="card-text",
+                   ),
+        ]
+    ),
+    className="w-100"
+)
 
 app = dash.Dash(
     external_stylesheets=[dbc.themes.BOOTSTRAP],
-
 )
 
-active_space = ""
-active_company = ""
+active_space = None
+active_company = None
 overdue = False
 
 selectSpace = dbc.InputGroup(
@@ -43,7 +68,11 @@ checkOverdue = dbc.InputGroup(
     [dbc.Checkbox(id="checkOverdue", label="Only Overdue")],
     className="mt-2")
 
-OpenTasksPerSpaceFig = px.bar(data_frame=open_tasks_per_space_data, x='space', y='count', color='space', hover_data=['space'])
+OpenTasksPerSpaceFig = px.bar(data_frame=open_tasks_per_space_data, x='space', y='count', color='space',
+                              hover_data=['space'])
+
+OpenTasksPerCompanyFig = px.bar(data_frame=open_tasks_per_company_data, x='company', y='count', color='company',
+                                hover_data=['company'])
 
 app.layout = dbc.Container(
     children=[
@@ -57,20 +86,22 @@ app.layout = dbc.Container(
         ),
         # charts
         dbc.Row(
-            [dbc.Col([dcc.Graph(figure=OpenTasksPerSpaceFig)], width=6),]
+            [], id="dashboard"
         ),
     ]
 )
 
 
 @app.callback(
-    Output(component_id='callback_output', component_property='children'),
+    Output(component_id='dashboard', component_property='children'),
     [Input(component_id='selectCompany', component_property='value'),
      Input(component_id='selectSpace', component_property='value'),
      Input(component_id='checkOverdue', component_property='value')]
 )
 def select_options(selected_company, selected_space, checked_overdue):
-    global active_space, active_company, overdue, open_tasks_per_space, open_tasks_per_space_data, open_overdue_tasks_per_space
+    global active_space, active_company, overdue, open_tasks_per_space_data, open_tasks_per_space, \
+        open_overdue_tasks_per_space, OpenTasksPerSpaceFig, space_overdue_tasks_card, company_overdue_tasks_card, \
+        open_tasks_per_company_data, OpenTasksPerCompanyFig
 
     ctx = dash.callback_context
     if ctx.triggered:
@@ -78,17 +109,71 @@ def select_options(selected_company, selected_space, checked_overdue):
 
         if input_id == "selectSpace":
             active_space = selected_space
+            overdue_count = open_overdue_tasks_per_space[(open_overdue_tasks_per_space.space == selected_space)].values[0][0]
+            space_overdue_tasks_card = dbc.Card(
+                dbc.CardBody(
+                    [
+                        html.H4("Space's Overdue Tasks", className="card-title"),
+                        html.P(html.Center(html.Strong(overdue_count)),
+                               className="card-text",
+                               ),
+                    ]
+                ),
+                className="w-100"
+            )
         elif input_id == "selectCompany":
             active_company = selected_company
+            overdue_count = open_overdue_tasks_per_company[(open_overdue_tasks_per_company.company == selected_company)].values[0][0]
+            company_overdue_tasks_card = dbc.Card(
+                dbc.CardBody(
+                    [
+                        html.H4("Company's Overdue Tasks", className="card-title"),
+                        html.P(html.Center(html.Strong(overdue_count)),
+                               className="card-text",
+                               ),
+                    ]
+                ),
+                className="w-100",
+            )
         elif input_id == "checkOverdue":
             overdue = not overdue
-            if overdue:
-                open_tasks_per_space_data = open_overdue_tasks_per_space
-            else:
+            if not overdue:
                 open_tasks_per_space_data = open_tasks_per_space
+                open_tasks_per_company_data = open_tasks_per_company
+            else:
+                open_tasks_per_space_data = open_overdue_tasks_per_space
+                open_tasks_per_company_data = open_overdue_tasks_per_company
+
+            OpenTasksPerSpaceFig = px.bar(data_frame=open_tasks_per_space_data, x='space', y='count', color='space',
+                                          hover_data=['space'])
+            OpenTasksPerCompanyFig = px.bar(data_frame=open_tasks_per_company_data, x='company', y='count', color='company',
+                                          hover_data=['company'])
+
+    return [
+        dbc.Col([html.Strong("Open tasks per space graph")], className="text-center mt-3 pt-3", width=6),
+        dbc.Col([html.Strong("Open tasks per company graph")], className="text-center mt-3 pt-3", width=6),
+        dbc.Col([dcc.Graph(figure=OpenTasksPerSpaceFig)], width=6),
+        dbc.Col([dcc.Graph(figure=OpenTasksPerCompanyFig)], width=6),
+        dbc.Col([space_overdue_tasks_card], width=5),
+        dbc.Col([company_overdue_tasks_card], width=5),
+        dbc.Col([
+            dbc.Button(id="btn_send_reminder", children=["Send Reminder"], className="w-100 h-100", n_clicks=0)
+        ], width=2)
+    ]
+
+@app.callback(
+    Output('callback_output', 'children'),
+    Input('btn_send_reminder', 'n_clicks'),
+)
+def send_reminder(reminder_btn):
+    changed_id = [p['prop_id'] for p in callback_context.triggered][0]
+
+    # reminder_btn = 1, as not to spam. If the user chooses a space or a company, this counter is reset and therefore
+    # can send another reminder to specific space or company through active_space, active_company
+    if 'btn_send_reminder' in changed_id and reminder_btn == 1:
+        print("Sent Reminder!")
 
     return ""
-
 
 if __name__ == "__main__":
     # OpenTasksPerSpace = reporter.task_count_by_space()

c614112acae8485180377df93e94eb4ad73ee2a0
BBuhlAir <buhl@buhl-consulting.com.cy>
2022-06-11 18:51:10 +0300

    Datatable callback demo

--- dashboard.py
+++ dashboard.py
@@ -66,5 +66,69 @@ def send_reminder(reminder_btn):
     return ""
 
 
+@app.callback(
+    Output('datatable-row-ids-container', 'children'),
+    Input('datatable-row-ids', 'derived_virtual_row_ids'),
+    Input('datatable-row-ids', 'selected_row_ids'),
+    Input('datatable-row-ids', 'active_cell'))
+def update_graphs(row_ids, selected_row_ids, active_cell):
+    return
+    # # JUST AN EXAMPLE IMPLEMENTATION - NEEDS TO BE ADJUSTED TO Confluence-Task-Reporting!
+    # # SOURCE: https://dash.plotly.com/datatable/interactivity
+    #
+    # # When the table is first rendered, `derived_virtual_data` and
+    # # `derived_virtual_selected_rows` will be `None`. This is due to an
+    # # idiosyncrasy in Dash (unsupplied properties are always None and Dash
+    # # calls the dependent callbacks when the component is first rendered).
+    # # So, if `rows` is `None`, then the component was just rendered
+    # # and its value will be the same as the component's dataframe.
+    # # Instead of setting `None` in here, you could also set
+    # # `derived_virtual_data=df.to_rows('dict')` when you initialize
+    # # the component.
+    # selected_id_set = set(selected_row_ids or [])
+    #
+    # if row_ids is None:
+    #     dff = df
+    #     # pandas Series works enough like a list for this to be OK
+    #     row_ids = df['id']
+    # else:
+    #     dff = df.loc[row_ids]
+    #
+    # active_row_id = active_cell['row_id'] if active_cell else None
+    #
+    # colors = ['#FF69B4' if id == active_row_id
+    #           else '#7FDBFF' if id in selected_id_set
+    #           else '#0074D9'
+    #           for id in row_ids]
+    #
+    # return [
+    #     dcc.Graph(
+    #         id=column + '--row-ids',
+    #         figure={
+    #             'data': [
+    #                 {
+    #                     'x': dff['country'],
+    #                     'y': dff[column],
+    #                     'type': 'bar',
+    #                     'marker': {'color': colors},
+    #                 }
+    #             ],
+    #             'layout': {
+    #                 'xaxis': {'automargin': True},
+    #                 'yaxis': {
+    #                     'automargin': True,
+    #                     'title': {'text': column}
+    #                 },
+    #                 'height': 250,
+    #                 'margin': {'t': 10, 'l': 10, 'r': 10},
+    #             },
+    #         },
+    #     )
+    #     # check if column exists - user may have deleted it
+    #     # If `column.deletable=False`, then you don't
+    #     # need to do this check.
+    #     for column in ['pop', 'lifeExp', 'gdpPercap'] if column in dff
+    # ]
+
 if __name__ == "__main__":
     app.run_server(debug=True)
